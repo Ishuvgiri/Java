7c2fc49f4ed8d4650a738b648529edd264933afa
Ishuv <igneelcanibal@gmail.com>
2025-11-18 11:43:17 +0545

    yes

--- quest5.java
+++ quest5.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class quest5{
+public class quest5 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the length of the square: ");
-        double length = scanner.nextDouble();
-        double area = length * length;
-        System.out.println("The area of square is: " + area);
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the length of the square: ");
+            double length = scanner.nextDouble();
+            double area = length * length;
+            System.out.println("The area of square is: " + area);
+        }
     }
 
 }
